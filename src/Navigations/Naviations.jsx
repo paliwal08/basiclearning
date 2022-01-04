9a10a2a6cfce8fa145a0db97aa9b13c2278400ba
paliwal08 <b.paliwal08@gmail.com>
2022-01-04 17:34:23 +0530

    my first  commit

---
+++ src/Navigations/Naviations.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const Navigation=()=>
+{
+    return(
+        
+            <nav>
+                <div></div>
+                <ul>
+                    <li><Link to="home">Home</Link></li>
+                    <li><Link to="home">Home</Link></li>
+                </ul>
+            </nav>
+    
+    )
+}
