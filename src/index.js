c08ce2960f53e1583516d2149051df9bdedf86a8
paliwal08 <b.paliwal08@gmail.com>
2022-01-04 17:35:02 +0530

    my first commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from "../src/components/home"
-import Login from "../src/components/login"
+import Home from "../src/components/home/Home"
+import Login from "../src/components/login/Login"
 
 ReactDOM.render(
   // <React.StrictMode>
